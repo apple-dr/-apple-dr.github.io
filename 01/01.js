ef0bd6a6b4c8e0a6e6157752706be9ae81169513
keivn <68813495+apple-dr@users.noreply.github.com>
2021-07-11 12:26:43 +0800

    js12021711

--- 01/01.js
+++ 01/01.js
@@ -1,5 +1,5 @@
 /*alert("你好呀!歡迎來到我的網頁")
-prompt("enter you name")*/
+prompt("enter you name")
 //------------------------------------------
 //var為變量宣告的專有名詞
 let x
@@ -9,3 +9,4 @@ var math
 math = 18
 console.log(math)
 
+*/
